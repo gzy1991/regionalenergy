50fe9f52b8ff8287b94306af649811abed8576b5
gzy <1522780701@qq.com>
2019-09-11 02:22:44 +0800

    页面框架修改：修改加载公共html时，延迟造成的bug

--- static/common/js/frame.js
+++ static/common/js/frame.js
@@ -1,7 +1,9 @@
 
 
+/*
 
-/*加载公共页面*/
+/!*加载公共页面*!/
  $(".headerpage").load("../static/common/html/header.html"); <!-- 顶部导航 -->
     $(".tpl-skiner").load("../static/common/html/skiner.html"); <!-- 风格切换 -->
     $(".left-sidebar").load("../static/common/html/sidebar.html");<!-- 侧边导航栏 -->
+*/
